40951f1fe76635f2415d195055542eea670525e4
moicben <benjamain.georges@gmail.com>
2025-04-23 20:40:42 +0700

    Fix Extact Product

--- mano-mano/extractProductPage.js
+++ mano-mano/extractProductPage.js
@@ -131,8 +131,8 @@ async function run() {
     }
 
     // Run tasks with a configurable number of browsers
-    const maxBrowsers = 5; 
-    const tabsPerBrowser = 4;
+    const maxBrowsers = 3; 
+    const tabsPerBrowser = 6;
     await runWithMultipleBrowsers(tasks, maxBrowsers, tabsPerBrowser);
 
     console.log("Product extraction complete.");
