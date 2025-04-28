0863ddcb7f5267cc957e9ac94bee2c0ec2ef0833
moicben <benjamain.georges@gmail.com>
2025-04-28 14:33:42 +0700

    Fixs Github Deploy

--- mano-mano/builder.js
+++ mano-mano/builder.js
@@ -21,7 +21,7 @@ async function main() {
     console.log(`Nombre de fichiers à traiter: ${files.length}`);
 
     // Étape 0 : Clonage du dépôt source
-    console.log("-> [0/6] Clonage du dépôt source <-");
+    console.log("-> [0/5] Clonage du dépôt source <-");
     await installRepository(sourceRepoDir, REPO_ORIGIN);
 
     //
@@ -32,41 +32,33 @@ async function main() {
         console.log(`\n=== Traitement du fichier ${fileName} (${i + 1}/${files.length}) ===`);
 
         // Étape 1 : Création du contenu shop via le JSON
-        console.log("-> [1/6] Création du contenu");
+        console.log("-> [1/5] Création du contenu");
         const shopObj = await createContent(fileName);
         const shop = shopObj.shopData;
 
-        // Étape 2 : Pré-indexation sur Google Search Console
-        console.log("-> [2/6] Pré-indexation Search Console");
-        try {
-            await indexSite(shop.domain);
-        } catch (err) {
-            console.error(`[2/6] Erreur lors de la pré-indexation '${shop.name}':`, err);
-        }
-
-        // Étape 3 : Upload du shop et contenus
-        console.log("-> [3/6] Upload sur Supabase");
+        // Étape 2 : Upload du shop et contenus
+        console.log("-> [2/5] Upload sur Supabase");
         // On passe un tableau contenant uniquement shopObj à uploadShop
         await uploadShop([shopObj]);
 
-        // Étape 4 : Déploiement Gituh Pages
-        console.log("-> [4/6] Déploiement sur Github");
+        // Étape 3 : Déploiement Gituh Pages
+        console.log("-> [3/5] Déploiement sur Github");
         try {
             await deployRepository(shop, sourceRepoDir);
         } catch (err) {
-            console.error(`[4/6] Erreur de création de propriété pour '${shop.name}':`, err);
+            console.error(`[3/5] Erreur de création de propriété pour '${shop.name}':`, err);
         }
 
-        // Étape 5 : Indexation via Google Search Console
-        console.log("-> [5/6] Indexation Search Console");
+        // Étape 4 : Indexation via Google Search Console
+        console.log("-> [4/5] Indexation Search Console");
         try {
             await indexSite(shop.domain);
         } catch (err) {
-            console.error(`[5/6] Erreur lors de l'indexation '${shop.name}':`, err);
+            console.error(`[4/5] Erreur lors de l'indexation '${shop.name}':`, err);
         }
 
-        // Étape 6 : Déplacement du fichier JSON traité dans le sous-répertoire /uploaded
-        console.log("-> [6/6] Déplacement du fichier JSON  <-");
+        // Étape 5 : Déplacement du fichier JSON traité dans le sous-répertoire /uploaded
+        console.log("-> [5/5] Déplacement du fichier JSON  <-");
         const uploadedDir = path.join(productsDir, "uploaded");
         if (!fs.existsSync(uploadedDir)) {
             fs.mkdirSync(uploadedDir);
@@ -76,7 +68,7 @@ async function main() {
         try {
             fs.renameSync(sourcePath, destinationPath);
         } catch (err) {
-            console.error(`[6/6] Erreur lors du déplacement du fichier '${fileName}':`, err);
+            console.error(`[6/5] Erreur lors du déplacement du fichier '${fileName}':`, err);
         }
     }
 }
