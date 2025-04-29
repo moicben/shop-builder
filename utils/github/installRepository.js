d019ec2f66c6b45a8906e7cab1dedb575fa423f9
moicben <benjamain.georges@gmail.com>
2025-04-29 10:06:30 +0700

    Paralleling Deploy

--- utils/github/installRepository.js
+++ utils/github/installRepository.js
@@ -6,9 +6,9 @@ export async function installRepository(sourceRepoDir, REPO_ORIGIN) {
     // Clone le repo source dans le dossier sourceRepoDir s'il n'existe pas
     if (!fs.existsSync(sourceRepoDir)) {
         console.log(`Clonage du dépôt ${sourceRepoDir}...`);
-        execCommand(`git clone ${REPO_ORIGIN} ${sourceRepoDir}`, { stdio: 'ignore' });
+        await execCommand(`git clone ${REPO_ORIGIN} ${sourceRepoDir}`, { stdio: 'ignore' });
         // Installation des dépendances dans le répertoire cloné sans changer le répertoire globalement
-        execCommand("npm install", { stdio: 'ignore', cwd: sourceRepoDir });
+        await execCommand("npm install", { stdio: 'ignore', cwd: sourceRepoDir });
     } else {
         console.log(`Le dépôt ${sourceRepoDir} existe déjà...`);
     }
